6524599b3f86ab3ba016830dd414d2bac0a79754
ptrodion <rdnpetrenko@gmail.com>
2023-06-29 23:02:57 +0100

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  listEl: document.querySelector('.gallery'),
+};
+
+function createGalleryItemsMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class ="gallery__item">
+          <a class ="gallery__link" href="${original}">
+            <img class ="gallery__image"
+             src="${preview}"
+              alt="${description}">
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+refs.listEl.insertAdjacentHTML(
+  'beforeend',
+  createGalleryItemsMarkup(galleryItems)
+);
+
+const lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
